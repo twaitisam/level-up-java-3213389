389bfb1821e0a1f83a16f196944f122ad3851c21
Sam Twaiti <136084213+twaitisam@users.noreply.github.com>
2025-07-30 09:14:37 +0000

    Implemented the Person class and the main method

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Person p1 = new Person("Sam", "Twaiti", 33);
+        Person p2 = new Person("Imran", "Twaiti", 6);
+        Person p3 = new Person("Rahaf", "Twaiti", 4);
+
+        p1.introduceYourSelf();
+        p2.introduceYourSelf();
+        p3.introduceYourSelf();
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,43 @@
+package com.linkedin.javacodechallenges;
 public class Person {
-  
+
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person(String firstName, String lastName, int age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  public void setFistName(String firstName){
+    this.firstName = firstName;
+  }
+
+  public String getFirstName(){
+    return this.firstName;
+  }
+
+  public void setLastName(String lastName){
+    this.lastName = lastName;
+  }
+
+  public String getLastName(){
+    return this.lastName;
+  }
+
+  public void setAge(int age){
+    this.age = age;
+  }
+
+  public int getAge(){
+    return this.age;
+  }
+
+  public void introduceYourSelf(){
+    System.out.println("Hi!! my name is " + this.firstName + " " + this.lastName + ".");
+    System.out.println("My age is: " + this.age);
+  }
+
 }
