a8361e38a8cce9fee109644bc2b97c9b07ca26ca
Sam Twaiti <136084213+twaitisam@users.noreply.github.com>
2026-02-24 02:34:38 +0000

    implemeted findLeastExpensive function and created the calculateCurrentPrince with its implementation

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
@@ -1,6 +1,7 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Optional;
 
 import lombok.AllArgsConstructor;
@@ -16,8 +17,11 @@ public class StoreItem {
   double discount;
 
   public static Optional<StoreItem> findLeastExpensive(Collection<StoreItem> items) {
-    // TODO: Implement
-    return Optional.empty();
+    return items.stream().min(Comparator.comparing(StoreItem::calculateCurrentPrice));
+  }
+
+  public double calculateCurrentPrice() {
+    return retailPrice - (retailPrice * discount);
   }
 
   @Override
